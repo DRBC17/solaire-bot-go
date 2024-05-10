855d228596ae304ab91e842be785428f5790d213
Diego Buezo <diegobuezo2016@gmail.com>
2024-05-10 16:09:30 -0600

    feat: created basic slash of create event

--- bot/bot.go
+++ bot/bot.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
+	"time"
 
 	env "github.com/DRBC17/solaire-bot-go/plugins"
 	"github.com/bwmarrin/discordgo"
@@ -44,10 +46,27 @@ var (
 				},
 			},
 		},
+		{
+			Name:        "crear-evento",
+			Description: "Crear un evento",
+		},
 	}
 
 	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 		"registrar": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			if i.ChannelID != env.Get("COMMANDS_CHANNEL_ID") {
+				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Flags:   discordgo.MessageFlagsEphemeral,
+						Content: "No puedes usar este comando en este canal.",
+					},
+				})
+
+				time.Sleep(time.Second * 10)
+
+				s.InteractionResponseDelete(i.Interaction)
+			}
 			options := i.ApplicationCommandData().Options
 			optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
 			for _, opt := range options {
@@ -100,6 +119,7 @@ var (
 				}
 
 				if memberFoundExist {
+					setNewMemberNickname(s, env.Get("GUILD_ID"), discord_member_id.Value.(string), albion_name.StringValue())
 					if existMemberRole(s, env.Get("GUILD_ID"), discord_member_id.Value.(string), env.Get("MEMBER_ROLE_ID")) {
 						s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 							// Ignore type for now, they will be discussed in "responses"
@@ -160,6 +180,45 @@ var (
 			}
 
 		},
+		"crear-evento": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseModal,
+				Data: &discordgo.InteractionResponseData{
+					CustomID: "create_event_" + i.Interaction.Member.User.ID,
+					Title:    "Crear evento",
+					Components: []discordgo.MessageComponent{
+						discordgo.ActionsRow{
+							Components: []discordgo.MessageComponent{
+								discordgo.TextInput{
+									CustomID:    "title",
+									Label:       "Titulo",
+									Style:       discordgo.TextInputShort,
+									Placeholder: "Ingrese el titulo del evento",
+									Required:    true,
+									MaxLength:   300,
+									MinLength:   5,
+								},
+							},
+						},
+						discordgo.ActionsRow{
+							Components: []discordgo.MessageComponent{
+								discordgo.TextInput{
+									CustomID:    "description",
+									Label:       "Descripción",
+									Style:       discordgo.TextInputParagraph,
+									Placeholder: "Ingrese la descripción del evento",
+									Required:    true,
+									MaxLength:   2000,
+									MinLength:   10,
+								}},
+						}},
+				},
+			})
+
+			if err != nil {
+				log.Fatalf("Error al crear el evento: %v", err)
+			}
+		},
 	}
 )
 
@@ -185,10 +244,79 @@ func setRole(s *discordgo.Session, guildID, memberID, roleID string) error {
 	return nil
 }
 
+func setNewMemberNickname(s *discordgo.Session, guildID, memberID, nickname string) error {
+	log.Println(nickname)
+	err := s.GuildMemberNickname(guildID, memberID, nickname)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func createGuildEvent(s *discordgo.Session, data discordgo.ModalSubmitInteractionData) error {
+	title := data.Components[0].(*discordgo.ActionsRow).Components[0].(*discordgo.TextInput).Value
+	description := data.Components[1].(*discordgo.ActionsRow).Components[0].(*discordgo.TextInput).Value
+
+	startingTime := time.Now().Add(1 * time.Hour)
+
+	endingTime := time.Now().Add(1 * time.Hour)
+
+	_, err := s.GuildScheduledEventCreate(env.Get("GUILD_ID"), &discordgo.GuildScheduledEventParams{
+		Name:               title,
+		Description:        description,
+		ScheduledStartTime: &startingTime,
+		ScheduledEndTime:   &endingTime,
+		EntityType:         discordgo.GuildScheduledEventEntityTypeVoice,
+		ChannelID:          env.Get("EVENT_CHANNEL_VOICE_ID"),
+		PrivacyLevel:       discordgo.GuildScheduledEventPrivacyLevelGuildOnly,
+	})
+	if err != nil {
+		log.Printf("Error creating scheduled event: %v", err)
+		return err
+	}
+	return nil
+}
+
 func init() {
 	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
-			h(s, i)
+		switch i.Type {
+		case discordgo.InteractionApplicationCommand:
+			if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+				h(s, i)
+			}
+		case discordgo.InteractionModalSubmit:
+			data := i.ModalSubmitData()
+
+			if !strings.HasPrefix(data.CustomID, "create_event") {
+				return
+			}
+
+			err := createGuildEvent(s, data)
+			if err != nil {
+				panic(err)
+			}
+
+			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Se ha creado el evento.",
+					Flags:   discordgo.MessageFlagsEphemeral,
+				},
+			})
+			if err != nil {
+				panic(err)
+			}
+			userid := strings.Split(data.CustomID, "_")[2]
+			_, err = s.ChannelMessageSend(env.Get("COMMANDS_CHANNEL_ID"), fmt.Sprintf(
+				"Evento creado por <@%s>\n\n**Evento**:\n%s\n\n**Descripción**:\n%s",
+				userid,
+				data.Components[0].(*discordgo.ActionsRow).Components[0].(*discordgo.TextInput).Value,
+				data.Components[1].(*discordgo.ActionsRow).Components[0].(*discordgo.TextInput).Value,
+			))
+			if err != nil {
+				panic(err)
+			}
+
 		}
 	})
 }
@@ -203,7 +331,6 @@ func Run() {
 		log.Fatalf("Cannot open the session: %v", err)
 	}
 
-	log.Println("Adding commands...")
 	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
 	for i, v := range commands {
 		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, env.Get("GUILD_ID"), v)
